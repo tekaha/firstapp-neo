9cb29d8aa1bf87241384962194781fc0d0f7c1bf
Holger Wingold <holger.wingold@adwizor.de>
2018-12-19 10:55:35 +0100

    Step 4 - Calling an OData Service with the Virtual Data Model

---
+++ application/src/main/java/de/adwizor/cloud/sdk/tutorial/BusinessPartnerServlet.java
@@ -0,0 +1,55 @@
+package de.adwizor.cloud.sdk.tutorial;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
+
+import com.sap.cloud.sdk.s4hana.datamodel.odata.helper.Order;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
+
+@WebServlet("/businesspartners")
+public class BusinessPartnerServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = CloudLoggerFactory.getLogger(BusinessPartnerServlet.class);
+
+    private static final String CATEGORY_PERSON = "1";
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            final List<BusinessPartner> businessPartners =
+                    new DefaultBusinessPartnerService()
+                            .getAllBusinessPartner()
+                            .select(BusinessPartner.BUSINESS_PARTNER,
+                                    BusinessPartner.LAST_NAME,
+                                    BusinessPartner.FIRST_NAME,
+                                    BusinessPartner.IS_MALE,
+                                    BusinessPartner.IS_FEMALE,
+                                    BusinessPartner.CREATION_DATE)
+                            .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
+                            .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
+                            .execute();
+
+            response.setContentType("application/json");
+            response.getWriter().write(new Gson().toJson(businessPartners));
+
+        } catch (final ODataException e) {
+            logger.error(e.getMessage(), e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(e.getMessage());
+        }
+    }
+}
